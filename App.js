1e26c44b268dd8c6d78ec290e5ec8130383b4dd1
Sarthak <sarthakbh321@gmail.com>
2020-04-16 02:35:35 +0530

    added code to get location

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 
 function App() {
+  const [state, changeState] = useState("Waiting")
+
+  const getLocation = async () => {
+    let {status} = await Permissions.askAsync(Permissions.LOCATION)
+
+    if(status !== 'granted') {
+      changeState("Provide Permission")
+    }
+
+    let options = {
+      accuracy: Location.Accuracy.High,
+      timeInterval: 5000
+    }
+
+    Location.watchPositionAsync(options, (data) => {
+      changeState(JSON.stringify(data))
+    })
+  }
+
+  useEffect(() => {
+    getLocation()
+  })
+
   return (
     <View style={styles.container}>
       <Navbar styles={styles.navbar}/>
-      <Text>Hello from App!</Text>
+      <Text>{state}</Text>
     </View>
   );
 }
@@ -15,7 +40,8 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     paddingTop: '1%',
-    paddingLeft: '5%',
+    paddingLeft: 20,
+    paddingRight: 20,
   },
   navbar: {
     width: '100%',
