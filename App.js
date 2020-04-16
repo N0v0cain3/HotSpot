804b597161b38be02b362230ac99bece981ac064
Sarthak <sarthakbh321@gmail.com>
2020-04-16 19:37:18 +0530

    added a map view

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import NetInfo from '@react-native-community/netinfo';
 import Navbar from "./components/Navbar";
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
 
 function App() {
-  const [state, changeState] = useState("Waiting")
+  const [latitude, changeLatitude] = useState("Waiting...")
+  const [longitude, changeLongitude] = useState("Waiting...")
+  const [carrier, setCarrier] = useState("Getting Carrier....")
 
   const getLocation = async () => {
     let {status} = await Permissions.askAsync(Permissions.LOCATION)
@@ -16,23 +19,42 @@ function App() {
     }
 
     let options = {
-      accuracy: Location.Accuracy.High,
+      accuracy: Location.Accuracy.Balanced,
       timeInterval: 5000
     }
 
     Location.watchPositionAsync(options, (data) => {
-      changeState(JSON.stringify(data))
+      changeLatitude(data.coords.latitude)
+      changeLongitude(data.coords.longitude)
+    })
+  }
+
+  const getCarrier = () => {
+    NetInfo.fetch().then(data => {
+      setCarrier(data.details.carrier)
     })
   }
 
   useEffect(() => {
     getLocation()
+    getCarrier()
   })
 
   return (
     <View style={styles.container}>
       <Navbar styles={styles.navbar}/>
-      <Text>{state}</Text>
+      <Text>Latitude: {latitude}</Text>
+      <Text>Longitude: {longitude}</Text>
+      <Text>Carrier: {carrier}</Text>
+      <MapView 
+        initialRegion={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0022,
+          longitudeDelta: 0.0021,
+        }}
+        style={styles.map}
+      />
     </View>
   );
 }
@@ -47,6 +69,10 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingTop: '5%',
     marginBottom: '5%',
+  },
+  map: {
+    width: '100%',
+    height: '80%',
   }
 });
 
